6e9867f627d9b04de2f67b5b3d2dd02a0e10ca76
Thibault Clerice <leponteineptique@gmail.com>
2014-10-28 13:07:42 +0100

    SimpleUser config

--- install/database.sql
+++ install/database.sql
@@ -2,6 +2,11 @@ CREATE DATABASE oAuthServer;
 GRANT USAGE ON *.* to perseids@localhost identified by 'perseids';
 GRANT ALL PRIVILEGES ON oAuthServer.* to perseids@localhost ;
 
+/*
+ *
+ * Largely inspired by https://github.com/jasongrimes/silex-simpleuser/blob/master/sql/mysql.sql
+ * 
+ */
 CREATE TABLE oAuthServer.`users` (
 	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(100) NOT NULL DEFAULT '',
@@ -12,4 +17,12 @@ CREATE TABLE oAuthServer.`users` (
 	`time_created` INT NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `unique_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE oAuthServer.`user_custom_fields` (
+user_id INT(11) UNSIGNED NOT NULL,
+attribute VARCHAR(50) NOT NULL DEFAULT '',
+value VARCHAR(255) DEFAULT NULL,
+PRIMARY KEY (user_id, attribute)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/app.php
+++ src/app.php
@@ -1,9 +1,76 @@
 <?php
 	$app = new Silex\Application(); 
+	
+	/* Register needed by authbucket oauth-2 */
 	$app->register(new AuthBucket\OAuth2\Provider\AuthBucketOAuth2ServiceProvider());
+	$app->register(new Silex\Provider\SecurityServiceProvider());
+	$app->register(new Silex\Provider\SerializerServiceProvider());
+	$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+	$app->register(new Silex\Provider\ValidatorServiceProvider());
+
+	/*Register Needed by SimpleUser*/
+	$app->register(new Silex\Provider\DoctrineServiceProvider());
+	$app->register(new Silex\Provider\SecurityServiceProvider());
+	$app->register(new Silex\Provider\RememberMeServiceProvider());
+	$app->register(new Silex\Provider\SessionServiceProvider());
+	$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+	$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+	$app->register(new Silex\Provider\TwigServiceProvider());
+	$app->register(new Silex\Provider\SwiftmailerServiceProvider());
+
+	$simpleUserProvider = new SimpleUser\UserServiceProvider();
+	$app->register($simpleUserProvider);
+
 
 	/* Debug Mode */
 	$app['debug'] = true;
+
+	/* Routes */
+	$app->mount('/user', $simpleUserProvider);
+
+	$app->get('/', function () use ($app) {
+	    return $app['twig']->render('index.twig', array());
+	});
+
+	$app['user.options'] = array();
+
+	/* TWIG Configuration */
+	$app['twig.path'] = array(__DIR__.'/../templates');
+	$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+	/* DATABASE CONFIGURATOR */
+	$app['db.options'] = array(
+		'driver' => 'pdo_mysql',
+		'host' => 'localhost',
+		'dbname' => 'oAuthServer',
+		'user' => 'perseids',
+		'password' => 'perseids',
+	);
+
+	// Mailer config. See http://silex.sensiolabs.org/doc/providers/swiftmailer.html
+	$app['swiftmailer.options'] = array();
+
+	/* Firewall configuration */
+	$app['security.firewalls'] = array(
+		/*
+		// Ensure that the login page is accessible to all
+		'login' => array(
+		'pattern' => '^/user/login$',
+		),*/
+		'secured_area' => array(
+			'pattern' => '^.*$',
+			'anonymous' => true,
+			'remember_me' => array(),
+			'form' => array(
+				'login_path' => '/user/login',
+				'check_path' => '/user/login_check',
+			),
+			'logout' => array(
+				'logout_path' => '/user/logout',
+			),
+			'users' => $app->share(function($app) { return $app['user.manager']; }),
+		),
+	);
 
 	return $app;
 ?>
