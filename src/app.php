10b49b26248a790d89b38e3e238ceaf9267f4a4c
Thibault Clerice <leponteineptique@gmail.com>
2014-10-29 23:35:32 +0100

    Structure modification

--- src/app.php
+++ src/app.php
@@ -1,15 +1,45 @@
-<?php
-	require_once __DIR__."/Controllers/Perseids/oAuth2.php";
-
-
-	foreach (glob(__DIR__."/Entity/*.php") as $filename)
-	{
-	    require_once $filename;
-	}
-	
+<?php	
 	$app = new Silex\Application(); 
+
+	$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+	    'security.firewalls' => array(
+			
+			// Ensure that the login page is accessible to all
+			'login' => array(
+			'pattern' => '^/user/login$',
+			),
+			'secured_area' => array(
+				'pattern' => '^.*$',
+				'anonymous' => true,
+				'remember_me' => array(),
+				'form' => array(
+					'login_path' => '/user/login',
+					'check_path' => '/user/login_check',
+				),
+				'logout' => array(
+					'logout_path' => '/user/logout',
+				),
+
+
+				'users' => $app->share(function($app) { return $app['user.manager']; }),
+			),
+			'oauth2_authorize' => array(
+				'pattern' => '^/rest/oauth2/authorize$',
+				'http' => true,
+				'users' => $app->share(function($app) { return $app['user.manager']; }), #We reuse the stuff from SimpleUser
+			),
+			'oauth2_token' => array(
+		        'pattern' => '^/rest/oauth2/token$',
+		        'oauth2_token' => true,
+		    ),
+		    'oauth2_debug' => array(
+				'pattern' => '^/rest/oauth2/debug$',
+				'oauth2_resource' => true,
+			),
+		)
+	));
+
 	/* Register needed by authbucket oauth-2 */
-	$app->register(new Silex\Provider\SecurityServiceProvider());
 	$app->register(new Silex\Provider\SerializerServiceProvider());
 	$app->register(new Silex\Provider\ServiceControllerServiceProvider());
 	$app->register(new Silex\Provider\ValidatorServiceProvider());
@@ -23,91 +53,57 @@
 	$app->register(new Silex\Provider\SwiftmailerServiceProvider());
 
 
+	$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+		'db.options' => require_once __DIR__."/Config/db.php",
+	));
+
 
 	/* SimpleUser Instance */
 	$simpleUserProvider = new SimpleUser\UserServiceProvider();
 	$app->register($simpleUserProvider);
 
-	/* oAuth2 Instance*/
-	$oAuth = new Perseids\OAuth2();
-	$app->register($oAuth);
-	require_once __DIR__."/Config/ORM.php";
+	//$app["security.authentication_providers"] = array($app['user.manager']);
 
 	/* oAuth2 Instance*/
 	$clients = new Perseids\ClientsManager\ClientServiceProvider($app);
 	$app->register($clients);
+
+	/* oAuth2 Instance*/
+	$AuthBucket = new AuthBucket\OAuth2\Provider\AuthBucketOAuth2ServiceProvider();
+	$app->register($AuthBucket);
+
+	$oAuth = new Perseids\OAuth2\OAuth2ServiceProvider($app, $app['user.manager']);
+	$app->register($oAuth);
+	
 
 
 	/* Debug Mode */
 	$app['debug'] = true;
 
 	/* Routes */
-	$app->mount('/rest/oauth2', $oAuth);
+	$app->mount('/api/oauth2', $oAuth);
 	$app->mount('/clients', $clients);
 	$app->mount('/user', $simpleUserProvider);
 
 	$app->get('/', function () use ($app) {
 	    return $app['twig']->render('index.twig', array());
 	});
-
-
+/*
+	if($app["security"]->getToken()) {
+		exit();	
+	}*/
 	/* TWIG Configuration */
 	$app['twig.path'] = array(__DIR__.'/../templates');
 	$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
-	#$app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
 
 	/* DATABASE CONFIGURATOR */
 
-
-	//Security Encoder for it
-	/*
-	$app['security.encoder.digest'] = $app->share(function ($app) {
-		return new PlaintextPasswordEncoder();
-	});
-	*/
 	// Mailer config. See http://silex.sensiolabs.org/doc/providers/swiftmailer.html
 	$app['swiftmailer.options'] = array();
 
-	/* Firewall configuration */
-	$app['security.firewalls'] = array(
-		/*
-		// Ensure that the login page is accessible to all
-		'login' => array(
-		'pattern' => '^/user/login$',
-		),*/
-		'secured_area' => array(
-			'pattern' => '^.*$',
-			'anonymous' => true,
-			'remember_me' => array(),
-			'form' => array(
-				'login_path' => '/user/login',
-				'check_path' => '/user/login_check',
-			),
-			'logout' => array(
-				'logout_path' => '/user/logout',
-			),
-
-
-			'users' => $app->share(function($app) { return $app['user.manager']; }),
-		),
-		'oauth2_authorize' => array(
-			'pattern' => '^/rest/oauth2/authorize$',
-			'http' => true,
-			'users' => $app->share(function($app) { return $app['user.manager']; }), #We reuse the stuff from SimpleUser
-		),
-		'oauth2_token' => array(
-	        'pattern' => '^/rest/oauth2/token$',
-	        'oauth2_token' => true,
-	    ),
-	    'oauth2_debug' => array(
-			'pattern' => '^/rest/oauth2/debug$',
-			'oauth2_resource' => true,
-		),
-	);
-
 	$app['security.access_rules'] = array(
-	    array('^/clients', 'ROLE_ADMIN', 'http')/*,
-	    array('^.*$', 'ROLE_USER'),*/
+	    array('^/clients', 'ROLE_ADMIN', 'http'),
+	    array('^.*$', 'ROLE_USER'),/**/
 	);
 
 	$app['user.passwordStrengthValidator'] = $app->protect(function(SimpleUser\User $user, $password) {
