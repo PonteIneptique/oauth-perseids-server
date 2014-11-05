a9e7696493e5ee12024da9169e5f494964a5d610
Thibault Clérice <leponteineptique@gmail.com>
2014-11-05 10:45:27 +0100

    AWS RDS DB CONFIG

--- src/config/db.php
+++ src/config/db.php
@@ -1,8 +1,9 @@
 <?php
 return array(
 	'driver' => 'pdo_mysql',
-	'host' => 'localhost',
-	'dbname' => 'oAuthServer',
-	'user' => 'perseids',
-	'password' => 'perseids',
+	'port' => $_SERVER['RDS_PORT'],
+	'host' => $_SERVER['RDS_HOSTNAME'],
+	'dbname' => $_SERVER['RDS_DB_NAME'],
+	'user' => $_SERVER['RDS_USERNAME'],
+	'password' => $_SERVER['RDS_PASSWORD'],
 );
