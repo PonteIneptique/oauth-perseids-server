abc9caaad9b70b99efac6db99bc4f0f96acb8408
Thibault Clérice <leponteineptique@gmail.com>
2014-11-05 10:17:31 +0100

    Upgraded Database generation

--- install/database.sql
+++ install/database.sql
@@ -1,3 +1,5 @@
-CREATE DATABASE oAuthServer;
+CREATE DATABASE oAuthServer
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 GRANT USAGE ON *.* to perseids@localhost identified by 'perseids';
 GRANT ALL PRIVILEGES ON oAuthServer.* to perseids@localhost ;
