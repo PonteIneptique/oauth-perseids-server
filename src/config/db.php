77f3ebb478fd6971ce4abe1f72ee2d3090bf3d45
Thibault Clérice <leponteineptique@gmail.com>
2014-11-28 13:46:31 +0100

    Going SQLITE

--- src/config/db.php
+++ src/config/db.php
@@ -1,9 +1,5 @@
 <?php
 return array(
-	'driver' => 'pdo_mysql',
-	'host' => 'localhost',
-	'port' => '3306',
-	'dbname' => 'oAuthServer',
-	'user' => 'perseids',
-	'password' => 'perseids',
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__.'/../cache/'.$app['env'].'/.ht.sqlite',
 );
