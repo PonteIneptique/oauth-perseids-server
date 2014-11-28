ab0e88ffe40980bcbe2d10bfdfd57622bf5895b4
Thibault Clérice <leponteineptique@gmail.com>
2014-11-28 12:00:51 +0100

    Updated status for auth bucket

--- src/config/db.php
+++ src/config/db.php
@@ -1,9 +1,17 @@
 <?php
 return array(
+	/*
 	'driver' => 'pdo_mysql',
 	'port' => $_SERVER['RDS_PORT'],
 	'host' => $_SERVER['RDS_HOSTNAME'],
 	'dbname' => $_SERVER['RDS_DB_NAME'],
 	'user' => $_SERVER['RDS_USERNAME'],
 	'password' => $_SERVER['RDS_PASSWORD'],
+	*/
+	'driver' => 'pdo_mysql',
+	'host' => 'localhost',
+	'port' => '3306',
+	'user' => 'perseids',
+	'password' => 'perseids',
+	'dbname' => "oAuthServer"
 );
