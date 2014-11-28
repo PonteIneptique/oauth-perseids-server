a489e11bb34a85a7ac3098c972540558ee04de4a
Thibault Clérice <leponteineptique@gmail.com>
2014-11-28 17:55:38 +0100

    Removed useless files

--- cmd/database.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE oAuthServer
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
-GRANT USAGE ON *.* to perseids@localhost identified by 'perseids';
-GRANT ALL PRIVILEGES ON oAuthServer.* to perseids@localhost ;
